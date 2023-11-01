84ce5a3e9c355b5d0ed4172078a240bf15ce6674
beomsu1 <beumsu1221@gmail.com>
2023-11-01 17:17:47 +0900

    Board

--- src/main/java/org/bs/jpa/controller/rest/BoardRestController.java
+++ src/main/java/org/bs/jpa/controller/rest/BoardRestController.java
@@ -24,7 +24,7 @@ import lombok.extern.log4j.Log4j2;
 @Log4j2
 @RequiredArgsConstructor
 @RequestMapping("/api/board/")
-public class RestBoardController {
+public class BoardRestController {
     
     private final BoardService boardService;
 
