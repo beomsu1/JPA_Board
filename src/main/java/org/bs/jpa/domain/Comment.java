268d29ecae9ee78102a40252302f45fad25364e9
beomsu1 <beumsu1221@gmail.com>
2023-11-02 12:08:10 +0900

    Comment

--- src/main/java/org/bs/jpa/domain/Comment.java
+++ src/main/java/org/bs/jpa/domain/Comment.java
@@ -14,6 +14,7 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.Table;
@@ -61,7 +62,8 @@ public class Comment {
         this.regDate = formattedDateTime;
     }
 
-    @ManyToOne(fetch = FetchType.LAZY) 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "bno")
     public Board board; // FK(Board)가 관계의 주인 -> ManyToOne
 
     // 댓글 수정
