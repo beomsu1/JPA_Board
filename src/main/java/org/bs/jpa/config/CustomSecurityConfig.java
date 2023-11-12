da65aa6eda04f8656446e36ae2e42745c57b91d7
beomsu1 <beumsu1221@gmail.com>
2023-11-12 23:07:28 +0900

    security ~

--- src/main/java/org/bs/jpa/config/CustomSecurityConfig.java
+++ src/main/java/org/bs/jpa/config/CustomSecurityConfig.java
@@ -2,8 +2,10 @@ package org.bs.jpa.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class CustomSecurityConfig {
@@ -13,5 +15,13 @@ public class CustomSecurityConfig {
 
         return new BCryptPasswordEncoder();
     }
-    
+
+    public SecurityFilterChain filterChain(final HttpSecurity http) throws Exception {
+
+        http.formLogin(config -> config.loginPage("/member/login"));
+
+        return http.build();
+
+    }
+
 }
